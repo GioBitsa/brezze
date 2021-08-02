2e308a80d266d06f63cddd802584288940415bd7
Giorgi <giorgi.bitsadze01@gmail.com>
2021-08-02 19:25:07 +0400

    .

--- app.js
+++ app.js
@@ -24,20 +24,11 @@ const sliderContent = document.querySelectorAll('#slider .column')
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
 let direction;
-let count = 1;
 
 next.addEventListener('click', function() {
   direction = -1;
   // carousel.style.justifyContent = 'flex-start';
   slider.style.transform = 'translate(-33%)';
-  count += 1;
-  console.log(count);
-  if (count < 3){
-    checkActive(sliderContent[count])
-  }else{
-    count = count % 3;
-    checkActive(sliderContent[count])
-  }
 });
 
 prev.addEventListener('click', function() {
@@ -47,14 +38,6 @@ prev.addEventListener('click', function() {
   }
   // carousel.style.justifyContent = 'flex-end';    
   slider.style.transform = 'translate(33%)';
-  count += 1;
-  console.log(count);
-  if (count < 3){
-    checkActive(sliderContent[count])
-  }else{
-    count = count % 3;
-    checkActive(sliderContent[count])
-  }
 });
 
 slider.addEventListener('transitionend', function() {
@@ -74,11 +57,7 @@ slider.addEventListener('transitionend', function() {
 }, false);
 
 
-function checkActive(index){
-  sliderContent.forEach(slider => {
-    slider.classList.remove('active');
-  })
-  index.classList.add('active');
-}
 
 // end of section 5 slider in index html
+
+
