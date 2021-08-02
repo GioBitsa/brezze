dc26d76879ea3ef6e16159add68866744daf0b27
Giorgi <giorgi.bitsadze01@gmail.com>
2021-08-02 17:12:12 +0400

    added slider

--- app.js
+++ app.js
@@ -15,253 +15,47 @@ close.addEventListener('click', () => {
 })
 
 
-// section 2 slider index html
+// section 5 slider in index html
 
+const carousel = document.getElementById('carousel');
+const slider = document.getElementById('slider');
 
-const numbers = document.querySelectorAll('#section-2 .numbers span');
-const sliders = document.querySelectorAll('#section-2 .content .project-slider');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+let direction;
 
-const num1 = document.getElementById('num1');
-const num2 = document.getElementById('num2');
-const num3 = document.getElementById('num3');
+next.addEventListener('click', function() {
+  direction = -1;
+  // carousel.style.justifyContent = 'flex-start';
+  slider.style.transform = 'translate(-33%)'; 
+  console.log(direction); 
+});
 
-const slider1 = document.getElementById('slider-1')
-const slider2 = document.getElementById('slider-2')
-const slider3 = document.getElementById('slider-3')
-
-document.addEventListener("DOMContentLoaded", function() {
-  num1.classList.add('active');
-
-  if (window.innerWidth < 900){
-    slider1.style.opacity = 1;
-    slider2.style.opacity = 0;
-    slider3.style.opacity = 0;
+prev.addEventListener('click', function() {
+  if (direction === -1) {
+    direction = 1;
+    slider.appendChild(slider.firstElementChild);
   }
+  // carousel.style.justifyContent = 'flex-end';    
+  slider.style.transform = 'translate(33%)';  
   
 });
 
-
-num1.onclick = () => {
-
-  // slide
-
-  slider1.style.transform = 'translateX(0)';
-  slider2.style.transform = 'translateX(200%)';
-  slider3.style.transform = 'translateX(400%)';
+slider.addEventListener('transitionend', function() {
+  // get the last element and append it to the front
   
-  // end of slide
-
-  checkActiveSlider(slider1)
-
-  // checking dots
-
-  checkNums(num1);
-
-  // end of checking dots
-
-}
-
-
-num2.onclick = () => {
-
-  // slide
-
-  slider1.style.transform = 'translateX(-200%)';
-  slider2.style.transform = 'translateX(0)';
-  slider3.style.transform = 'translateX(200%)';
-
-
-  // end of slide
-
-  checkActiveSlider(slider2)
-
-  // checking dots
-
-  checkNums(num2);
-
-  // end of checking dots
-
-}
-
-num3.onclick = () => {
-
-  // slide
-
-  slider1.style.transform = 'translateX(-400%)';
-  slider2.style.transform = 'translateX(-200%)';
-  slider3.style.transform = 'translateX(0)';
-
-
-  checkActiveSlider(slider3)
-
-  // end of slide
-
-  // checking dots
-
-  checkNums(num3);
-
-  // end of checking dots
-
-}
-
-
-// end of section 2 slider index html
-
-
-// testimonials slider
-
-const dots = document.querySelectorAll('.dots span');
-const columns = document.querySelectorAll('.testimonials .column')
-
-const dot1 = document.getElementById('dot1');
-const dot2 = document.getElementById('dot2');
-const dot3 = document.getElementById('dot3');
-
-const column1 = document.getElementById('column1');
-const column2 = document.getElementById('column2');
-const column3 = document.getElementById('column3');
-
-document.addEventListener("DOMContentLoaded", function() {
-  dot2.classList.add('active');
-
-  if (window.innerWidth < 900){
-    column1.style.opacity = 0;
-    column2.style.opacity = 1;
-    column3.style.opacity = 0;
+  if (direction === 1) {
+    slider.prepend(slider.lastElementChild);
+  } else {
+    slider.appendChild(slider.firstElementChild);
   }
   
-});
-
-
-// console.log(window.innerWidth);
-
-dot1.onclick = () => {
-
-  // slide
-
-  column1.style.transform = 'translateX(100%)';
-  column2.style.transform = 'translateX(100%)';
-  column3.style.transform = 'translateX(100%)';
-
-
-  if (window.innerWidth < 900){
-    column1.style.opacity = 1;
-    column2.style.opacity = 0;
-    column3.style.opacity = 0;
-  }
-
-  if (window.innerWidth < 800){
-    column1.style.transform = 'translateX(0)';
-    column2.style.transform = 'translateX(100%)';
-    column3.style.transform = 'translateX(100%)';
-  }
-
-  
-  
-  // end of slide
-
-  checkActiveColumn(column1)
-
-  // checking dots
-
-  checkDots(dot1);
-
-  // end of checking dots
-
-}
-
-dot2.onclick = () => {
-
-  // slide
-
-  column1.style.transform = 'translateX(0)';
-  column2.style.transform = 'translateX(0)';
-  column3.style.transform = 'translateX(0)';
-
-
-  if (window.innerWidth < 900){
-    column1.style.opacity = 0;
-    column2.style.opacity = 1;
-    column3.style.opacity = 0;
-  }
-
-  if (window.innerWidth < 800){
-    column1.style.transform = 'translateX(0)';
-    column2.style.transform = 'translateX(0)';
-    column3.style.transform = 'translateX(100%)';
-  }
-
-  
-
-  // end of slide
-
-  checkActiveColumn(column2)
-
-  // checking dots
-
-  checkDots(dot2);
-
-  // end of checking dots
-
-}
-
-dot3.onclick = () => {
-
-  // slide
-
-  column1.style.transform = 'translateX(-100%)';
-  column2.style.transform = 'translateX(-100%)';
-  column3.style.transform = 'translateX(-100%)';
-
-
-  if (window.innerWidth < 900){
-    column1.style.opacity = 0;
-    column2.style.opacity = 0;
-    column3.style.opacity = 1;
-  }
-
-  if (window.innerWidth < 800){
-    column1.style.transform = 'translateX(0)';
-    column2.style.transform = 'translateX(100%)';
-    column3.style.transform = 'translateX(0)';
-  }
-
-  checkActiveColumn(column3)
-
-  // end of slide
-
-  // checking dots
-
-  checkDots(dot3);
-
-  // end of checking dots
-
-}
-
-function checkDots(index){
-  dots.forEach(dot => {
-    dot.classList.remove('active');
+  slider.style.transition = 'none';
+  slider.style.transform = 'translate(0)';
+  setTimeout(() => {
+    slider.style.transition = 'all 0.5s';
   })
-  index.classList.add('active');
-}
-function checkActiveSlider(index){
-  sliders.forEach(slider => {
-    slider.classList.remove('active');
-  })
-  index.classList.add('active');
-}
-function checkActiveColumn(index){
-  columns.forEach(column => {
-    column.classList.remove('active');
-  })
-  index.classList.add('active');
-}
-function checkNums(index){
-  numbers.forEach(numb => {
-    numb.classList.remove('active');
-  })
-  index.classList.add('active');
-}
+}, false);
 
 
-// end of testimonials slider
+// end of section 5 slider in index html
